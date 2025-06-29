ee0bfb3af0493e39a85392698108f975eb95cd1d
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-29 05:53:20 -1200

    Dynamic Programming implement Fibonacci Sequence

---
+++ Dynamic programming/tabulate/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func fibonacciTabulate(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	if n <= 2 {
+		return 1
+	}
+	previous1 := 1
+	previous2 := 1
+	currentVal := 0
+	for i := 3; i <= n; i++ {
+		currentVal = previous1 + previous2
+		previous1 = previous2
+		previous2 = currentVal
+	}
+	return currentVal
+
+}
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21,...
+func main() {
+	fmt.Println(fibonacciTabulate(100))
+}
