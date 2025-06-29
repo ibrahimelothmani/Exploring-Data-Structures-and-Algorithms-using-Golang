343646e50234c437cbb00ff3c654751340c15c5e
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-28 17:51:30 -1200

    Refactor Circular Linked List operations for improved clarity and functionality

--- Circular linked list/main.go
+++ Circular linked list/main.go
@@ -9,78 +9,96 @@ type Node struct {
 }
 
 func printList(head *Node) {
+	if head == nil {
+		fmt.Println()
+		return
+	}
 	current := head
-	for current != nil {
+	for {
 		fmt.Print(current.Value, " ")
 		current = current.Next
+		if current == head {
+			break
+		}
 	}
 	fmt.Println()
 }
+
 func reverseList(head *Node) *Node {
-	var prev *Node
+	if head == nil {
+		return nil
+	}
 	current := head
-	for current != nil {
+	var prev *Node
+	for {
 		next := current.Next
-		current.Next = prev
-		current.Previous = next // Set Previous pointer for doubly linked list
+		current.Next = current.Previous
+		current.Previous = next
 		prev = current
 		current = next
+		if current == head {
+			break
+		}
 	}
 	return prev
 }
-func insertAtMiddle(head *Node, value int) *Node {
-	newNode := &Node{Value: value}
-	if head == nil {
-		return newNode
-	}
 
-	slow, fast := head, head
-	var prev *Node
-
-	for fast != nil && fast.Next != nil {
-		prev = slow
-		slow = slow.Next
-		fast = fast.Next.Next
-	}
-
-	if prev != nil {
-		prev.Next = newNode
-		newNode.Previous = prev
-	}
-	newNode.Next = slow
-	if slow != nil {
-		slow.Previous = newNode
-	}
-
-	return head
-}
 func insertAtStart(head *Node, value int) *Node {
 	newNode := &Node{Value: value}
 	if head == nil {
+		newNode.Next = newNode
+		newNode.Previous = newNode
 		return newNode
 	}
-
+	last := head.Previous
 	newNode.Next = head
+	newNode.Previous = last
 	head.Previous = newNode
+	last.Next = newNode
 	return newNode
 }
+
 func insertAtEnd(head *Node, value int) *Node {
 	newNode := &Node{Value: value}
 	if head == nil {
+		newNode.Next = newNode
+		newNode.Previous = newNode
 		return newNode
 	}
+	last := head.Previous
+	last.Next = newNode
+	newNode.Previous = last
+	newNode.Next = head
+	head.Previous = newNode
+	return head
+}
 
-	current := head
-	for current.Next != nil {
-		current = current.Next
+func insertAtMiddle(head *Node, value int) *Node {
+	if head == nil {
+		newNode := &Node{Value: value}
+		newNode.Next = newNode
+		newNode.Previous = newNode
+		return newNode
 	}
-	current.Next = newNode
-	newNode.Previous = current
+	slow, fast := head, head
+	for fast.Next != head && fast.Next.Next != head {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	newNode := &Node{Value: value}
+	next := slow.Next
+	slow.Next = newNode
+	newNode.Previous = slow
+	newNode.Next = next
+	next.Previous = newNode
 	return head
 }
 
 func main() {
 	head := &Node{Value: 1}
+	head.Next = head
+	head.Previous = head
+
 	head = insertAtEnd(head, 2)
 	head = insertAtEnd(head, 3)
 	head = insertAtStart(head, 0)
@@ -90,11 +108,11 @@ func main() {
 	printList(head)
 
 	fmt.Println("Reversed List:")
-	reversedHead := reverseList(head)
-	printList(reversedHead)
+	head = reverseList(head)
+	printList(head)
 
 	fmt.Println("List after inserting 4 at middle:")
-	head = insertAtMiddle(reversedHead, 4)
+	head = insertAtMiddle(head, 4)
 	printList(head)
 
 	fmt.Println("List after inserting 5 at start:")
@@ -104,4 +122,5 @@ func main() {
 	fmt.Println("List after inserting 6 at end:")
 	head = insertAtEnd(head, 6)
 	printList(head)
+
 }
